c1e337137d05a3ad90d561f6a6c3e41bcb97d1ee
Tomasz Hanc <tomasz.hanc@gmail.com>
2019-07-02 16:20:07 +0200

    Add failure cache and fallback logger for TelemetryClient

--- src/AppInsightsPHP/Client/Client.php
+++ src/AppInsightsPHP/Client/Client.php
@@ -5,6 +5,8 @@ declare (strict_types=1);
 namespace AppInsightsPHP\Client;
 
 use ApplicationInsights\Telemetry_Client;
+use Psr\Log\LoggerInterface;
+use Psr\SimpleCache\CacheInterface;
 
 /**
  * @method \ApplicationInsights\Telemetry_Context getContext()
@@ -19,22 +21,68 @@ use ApplicationInsights\Telemetry_Client;
  * @method void endRequest(\ApplicationInsights\Channel\Contracts\Request_Data $request, $durationInMilliseconds = 0, $httpResponseCode = 200, $isSuccessful = true, $properties = NULL, $measurements = NULL)
  * @method void trackException($ex, $properties = NULL, $measurements = NULL)
  * @method void trackDependency($name,$type = "",$commandName = NULL,$startTime = NULL,$durationInMilliseconds = 0,$isSuccessful = true,$resultCode = NULL,$properties = NULL)
- * @method void flush($options = array(), $sendAsync = false)
  */
 final class Client
 {
+    public const CACHE_CHANNEL_KEY = 'app_insights_php.failure_cache';
+    public const CACHE_CHANNEL_TTL_SEC = 86400; // 1 day
+
     private $client;
     private $configuration;
+    private $failureCache;
+    private $fallbackLogger;
 
-    public function __construct(Telemetry_Client $client, Configuration $configuration)
-    {
+    public function __construct(
+        Telemetry_Client $client,
+        Configuration $configuration,
+        CacheInterface $failureCache = null,
+        LoggerInterface $fallbackLogger = null
+    ) {
         $this->client = $client;
         $this->configuration = $configuration;
+        $this->failureCache = $failureCache;
+        $this->fallbackLogger = $fallbackLogger;
     }
 
     public function configuration(): Configuration
     {
         return $this->configuration;
+    }
+
+    public function flush(): void
+    {
+        try {
+            if ($this->failureCache && $this->failureCache->has(self::CACHE_CHANNEL_KEY)) {
+                $this->client->getChannel()->setQueue(
+                    array_merge(
+                        unserialize($this->failureCache->get(self::CACHE_CHANNEL_KEY)),
+                        $this->client->getChannel()->getQueue()
+                    )
+                );
+
+                $this->failureCache->delete(self::CACHE_CHANNEL_KEY);
+            }
+
+            $this->client->flush();
+        } catch (\Throwable $e) {
+            if ($this->failureCache) {
+                $queueContent = $this->client->getChannel()->getQueue();
+
+                if ($this->failureCache->has(self::CACHE_CHANNEL_KEY)) {
+                    $previousQueueContent = unserialize($this->failureCache->get(self::CACHE_CHANNEL_KEY));
+                    $queueContent = array_merge($previousQueueContent, $queueContent);
+                }
+
+                $this->failureCache->set(self::CACHE_CHANNEL_KEY, serialize($queueContent), self::CACHE_CHANNEL_TTL_SEC);
+            }
+
+            if ($this->fallbackLogger) {
+                $this->fallbackLogger->error(
+                    sprintf('Exception occurred while flushing App Insights Telemetry Client: %s', $e->getMessage()),
+                    json_decode($this->client->getChannel()->getSerializedQueue(), true)
+                );
+            }
+        }
     }
 
     public function __call($name, $arguments)
--- src/AppInsightsPHP/Client/ClientFactory.php
+++ src/AppInsightsPHP/Client/ClientFactory.php
@@ -5,16 +5,26 @@ declare (strict_types=1);
 namespace AppInsightsPHP\Client;
 
 use ApplicationInsights\Telemetry_Client;
+use Psr\Log\LoggerInterface;
+use Psr\SimpleCache\CacheInterface;
 
 final class ClientFactory implements ClientFactoryInterface
 {
     private $instrumentationKey;
     private $configuration;
+    private $failureCache;
+    private $fallbackLogger;
 
-    public function __construct(string $instrumentationKey, Configuration $configuration)
-    {
+    public function __construct(
+        string $instrumentationKey,
+        Configuration $configuration,
+        CacheInterface $failureCache = null,
+        LoggerInterface $fallbackLogger = null
+    ) {
         $this->instrumentationKey = $instrumentationKey;
         $this->configuration = $configuration;
+        $this->failureCache = $failureCache;
+        $this->fallbackLogger = $fallbackLogger;
     }
 
     public function create() : Client
@@ -22,6 +32,11 @@ final class ClientFactory implements ClientFactoryInterface
         $client = new Telemetry_Client();
         $client->getContext()->setInstrumentationKey($this->instrumentationKey);
 
-        return new Client($client, $this->configuration);
+        return new Client(
+            $client,
+            $this->configuration,
+            $this->failureCache,
+            $this->fallbackLogger
+        );
     }
 }
